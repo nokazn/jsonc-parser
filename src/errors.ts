9ec21142816774bc399e45b84701191e0c689b15
nokazn <41154684+nokazn@users.noreply.github.com>
2024-01-20 15:51:13 +0900

    tmp

---
+++ src/errors.ts
@@ -0,0 +1,11 @@
+export class ExpectedColonError extends Error {
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+  }
+}
+
+export class ExpectedCommaError extends Error {
+  constructor(message?: string, options?: ErrorOptions) {
+    super(message, options);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
-console.log("Hello World!");
+const TOKEN = Object.freeze({
+  START_OBJECT: "{",
+  END_OBJECT: "{",
+  START_ARRAY: "[",
+  END_ARRAY: "]",
+  COMMA: ",",
+  SPACE: " ",
+});
+
+type Token = typeof TOKEN & {
+  // VALUE:
+};
+
+class JsoncParse {
+  count = 0;
+  constructor() {}
+
+  parse(input: string) {}
+
+  #readNext() {
+    this.count += 1;
+  }
+  #skipWhitespace() {}
+  #eatColon() {}
+
+  #parseObject(input: string) {
+    if (input[this.count] === TOKEN.START_OBJECT) {
+      this.#readNext();
+      while (input[this.count] !== TOKEN.END_OBJECT) {
+        const key = this.#parseString();
+        this.#skipWhitespace();
+        this.#eatColon();
+      }
+    }
+  }
+
+  #parseValue() {}
+  #parseArray() {}
+  #parseString() {}
+  #parseNumber() {}
+}
+
+export { parseJsonc };
